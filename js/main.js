fca08e72ed18ddf15f30fb8b3c4da1b94cc72d03
davedavehong <davedavehong@qq.com>
2016-07-26 20:05:21 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/**
+ * Created by dave on 7月26 026.
+ */
+var sly;
+jQuery(function ($) {
+    'use strict';
+
+    // ==========================================================================
+    //   Header example
+    // ==========================================================================
+    var $example = $('#example');
+    var $frame = $example.find('.frame');
+    sly = new Sly($frame, {
+        horizontal: 1,
+        itemNav: 'forceCentered',
+        activateMiddle: 1,
+        smart: 1,
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: null,
+        scrollBar: $example.find('.scrollBar'),
+        scrollBy: 1,
+        activatePageOn: 'click',
+        speed: 200,
+        moveBy: 600,
+        elasticBounds: 1,
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1,
+
+        //自动循环
+        cycleBy: "pages",
+        cycleInterval: 1000,
+        pauseOnHover: true,
+        startPaused: false,
+
+        // Buttons
+        forward: $example.find('.forward'),
+        backward: $example.find('.backward'),
+        prev: $example.find('.prev'),
+        next: $example.find('.next'),
+        prevPage: $example.find('.prevPage'),
+        nextPage: $example.find('.nextPage')
+    }).init();
+
+
+
+    // Method calling buttons
+    $example.on('click', 'button[data-action]', function () {
+        var action = $(this).data('action');
+
+        switch (action) {
+            case 'add':
+                sly.add('<li>' + sly.items.length + '</li>');
+                break;
+            case 'remove':
+                sly.remove(-1);
+                break;
+            default:
+                sly[action]();
+        }
+    });
+
+    sly.on('active', function (eventName,itemIndex) {
+        console.log(eventName+"="+itemIndex);
+    });
+});
